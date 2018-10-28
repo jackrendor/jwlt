365a012e4dc211bfd9163e9a3347729863625913
Jack Rendor <jackrendor@gmail.com>
2018-10-28 09:37:00 +0100

    Cosmetic fix: tabs set to 4 spaces, removed useless brackets

--- functions.h
+++ functions.h
@@ -4,96 +4,95 @@
 
 void usage(char *file_name)
 {
-  printf("Usage:\n");
-  printf("   %s\t <charset string> <wordlist lenght> <save interval>\n", file_name);
-  printf("   %s\t <charset string> <wordlist lenght>\n", file_name);
-  printf("   %s\t [--delete-session / -ds]\n", file_name);
+    printf("Usage:\n");
+    printf("   %s\t <charset string> <wordlist lenght> <save interval>\n", file_name);
+    printf("   %s\t <charset string> <wordlist lenght>\n", file_name);
+    printf("   %s\t [--delete-session / -ds]\n", file_name);
 }
 
 int save_progress(int *tracker, int tracker_size)
 {
-  // Open file in write mode
-  FILE *session = fopen(SESSION_FILENAME, "w");
+    // Open file in write mode
+    FILE *session = fopen(SESSION_FILENAME, "w");
 
-  // Check status of file stream
-  if (session == NULL){
-    return -1;
-  }
-
-  for (int i=0; i<tracker_size; i++) {
-    // write & check
-    if (fprintf(session, "%d-", tracker[i]) <= 0){
-      fclose(session);
-      return -2;
+    // Check status of file stream
+    if (session == NULL){
+        return -1;
     }
-  }
 
-  // Everything went fine (I hope.)
-  fclose(session);
-  return 1;
+    for (int i=0; i<tracker_size; i++) {
+        // write & check
+        if (fprintf(session, "%d-", tracker[i]) <= 0){
+            fclose(session);
+            return -2;
+        }
+    }
+
+    // Everything went fine (I hope.)
+    fclose(session);
+    return 1;
 }
 
 void encrease(int *tracker, int tracker_size, int pattern_len)
 {
-  /*
-    This function increase by one the array that
-    indexes the charset when printing a char
-  */
-  tracker[tracker_size-1]++;
-  for (int i=tracker_size-1; i>=0; --i){
+    /*
+      This function increase by one the array that
+      indexes the charset when printing a char
+    */
 
-    if (tracker[i] >= pattern_len){
-      tracker[i] = 0;
-
-      // We don't want to add stuff at -1 index
-      if (i-1 >= 0)
-        tracker[i-1]++;
+    tracker[tracker_size-1]++;
+    for (int i=tracker_size-1; i>=0; --i){  
+      
+        if (tracker[i] >= pattern_len){
+            tracker[i] = 0; 
+            // We don't want to add stuff at -1 index
+            if (i-1 >= 0)
+                tracker[i-1]++;
+        } 
+    
     }
-
-  }
 }
 
 void print_gen(char *pattern, int *tracker, int tracker_size)
 {
-  /*
-    This function prints the word char by char
-    based on the tracker array that contains
-    all the indices of the charset
-  */
-  for (int c=0; c<tracker_size; c++){
-    printf("%c", pattern[tracker[c]]);
-  }
-  printf("\n");
+    /*
+        This function prints the word char by char
+        based on the tracker array that contains
+        all the indices of the charset
+    */
+    for (int c=0; c<tracker_size; c++)
+        printf("%c", pattern[tracker[c]]);
+    
+    printf("\n");
 }
 
 int reload_session(int *tracker, int tracker_size)
 {
-  /*
-    This function reads from the file and
-    updated the tracker (or index) to the
-    last saved session
-  */
-  FILE *session = fopen(SESSION_FILENAME, "r");
-  if (session == NULL)
-    return 0;
+    /*
+      This function reads from the file and
+      updated the tracker (or index) to the
+      last saved session
+    */
 
-  /*
-    TODO: Find a workaround to allocate in the
-          right way the container.
-    NOTE: I hate this shit. This is prone to buffer overflow.
-  */
-  char container[50];
+    FILE *session = fopen(SESSION_FILENAME, "r");
+    if (session == NULL)
+        return 0; 
+    /*
+        TODO: Find a workaround to allocate in the
+            right way the container.
+    */
+    size_t cont_n = 50;
+    char container[cont_n]; 
+    // Get one line. It's good enough.
+    if (fgets(container, cont_n, session) == NULL){
+        fclose(session);
+        return -1;
+    }
 
-  // Get one line. It's good enough.
-  if (fgets(container, (size_t)50, session) == NULL){
+    // Update the tracker (or index)
+    for (int i=0; i<tracker_size; i++)
+        tracker[i] = atoi(g_arg(container, i, '-'));
+    
     fclose(session);
-    return -1;
-  }
-
-  // Update the tracker (or index)
-  for (int i=0; i<tracker_size; i++){
-    tracker[i] = atoi(g_arg(container, i, '-'));
-  }
-  fclose(session);
-  return 1;
+    return 1;
 }
--- sfunctions.h
+++ sfunctions.h
@@ -10,39 +10,38 @@
 
 char *g_arg(char *string, int argument_pos, char placeholder)
 {
-  int c2 = 0, current_arg = 0;
-  /*
-    TODO: fix this shit. I don't want to allocate
-          too much space for allocating an argument
-          that it's (for example) 3 chars long.
-  */
-  char *tmp = (char*) malloc(strlen(string));
+    int c2 = 0, current_arg = 0;
+    /*
+        TODO: fix this shit. I don't want to allocate
+            too much space for allocating an argument
+            that it's (for example) 3 chars long.
+    */
+    char *tmp = (char*) malloc(strlen(string));
 
-  for (size_t c=0; c<strlen(string); c++){
-    if (string[c] == placeholder){
-      current_arg++;
-    }else if (current_arg == argument_pos){
-      tmp[c2] = string[c];
-      c2++;
+    for (size_t c=0; c<strlen(string); c++){
+        if (string[c] == placeholder){
+            current_arg++;
+        }else if (current_arg == argument_pos){
+            tmp[c2] = string[c];
+            c2++;
+        }
     }
-  }
-  return tmp;
+    return tmp;
 }
 int isnumber(char *argument)
 {
-  /*
-    Argument checker. I want to be sure
-    that a string is an integer.
-    I don't want to mess around.
-
-    NOTE: if it's a number, returns 1
-          otherwise, 0.
-  */
-
-  while (*arg != '\0'){
-    if (!isdigit(*arg))
-      return 0;
-    arg++;
-  }
-  return 1;
+    /*
+        Argument checker. I want to be sure
+        that a string is an integer.
+        I don't want to mess around.  
+        
+        NOTE: if it's a number, returns 1
+            otherwise, 0.
+    */  
+    while (*arg != '\0'){
+        if (!isdigit(*arg))
+            return 0;
+        arg++;
+    }
+    return 1;
 }
